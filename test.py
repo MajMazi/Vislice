71a2db2456b57f8c18b0ed86ed023e389f613651
Maj Mazi <maj.mazi@student.fmf.uni-lj.si>
2019-04-24 15:00:24 +0200

    Added test.py and changed readme.

---
+++ test.py
@@ -0,0 +1,11 @@
+# Napiši program, ki izpiše prvih 200 praštevil
+
+def (je_prastevilo):
+    for i in range(2,n):
+        in n % i == 0:
+        return False
+    return True
+
+for x in range(2, 201):
+    if je_prastevilo(x):
+        print(x)
